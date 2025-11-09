3b68fc4bb4d73b1afd7aacc650a98f40cd5ff211
Shaik Jakir Hussain <shaikjakeerhussain581@gmail.com>
2025-11-09 14:04:12 +0530

    Add stubs for getAll and getById in TalentService
    Introduced placeholder methods getAll and getById to TalentService for future implementation. These methods currently throw UnsupportedOperationException and will be completed in subsequent commits.

--- src/main/java/com/talentsupply/talentmanagement/service/TalentService.java
+++ src/main/java/com/talentsupply/talentmanagement/service/TalentService.java
@@ -1,8 +1,22 @@
 package com.talentsupply.talentmanagement.service;
 
+import java.util.List;
+
+import com.talentsupply.talentmanagement.entity.Talent;
+
 /**
  * Placeholder service for Talent business logic.
  */
 public class TalentService {
+
+    public List<Talent> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
     // TODO: implement service methods and inject repository
+
+    public Object getById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    }
 }
