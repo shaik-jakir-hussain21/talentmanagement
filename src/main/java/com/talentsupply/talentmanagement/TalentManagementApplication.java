813bbd30bc8648e4219caad52fef8c518360f3f9
Shaik Jakir Hussain <shaikjakeerhussain581@gmail.com>
2025-11-09 11:06:39 +0530

    Initial commit

---
+++ src/main/java/com/talentsupply/talentmanagement/TalentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.talentsupply.talentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentManagementApplication.class, args);
+	}
+
+}
